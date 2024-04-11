27b38873faf56f6dfb989e0225847a432032dab9
Amsharaj <aamsharaj@gmail.com>
2024-04-11 13:42:56 +0530

    update

--- frontend/src/pages/home/PopularClasses/PopularClasses.jsx
+++ frontend/src/pages/home/PopularClasses/PopularClasses.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import useAxiosFetch from '../../../hooks/useAxiosFetch'
-import Card from './card'
+import Card from './Card'
 
 const PopularClasses = () => {
     const axiosFecth=useAxiosFetch()
